9c5803c3f689c584bd2e3051d8744a6a91ad3f23
yurychu <yury_chugunov@mail.ru>
2020-10-19 14:26:01 +0300

    add parameters and check output

--- desktop_client/src/MxfSpecificParameters.cpp
+++ desktop_client/src/MxfSpecificParameters.cpp
@@ -6,6 +6,7 @@
 #include <mxf_specific_parameters_lib/MxfInfo.hpp>
 
 #include <iostream>
+#include <sstream>
 
 
 MxfSpecificParameters::MxfSpecificParameters()
@@ -13,8 +14,27 @@ MxfSpecificParameters::MxfSpecificParameters()
 {
     setupUi(this);
 
+    comboBox->clear();
+    comboBox->addItem("1.1"); //"1.1", "1.2", "1.3"
+    comboBox->addItem("1.2");
+    comboBox->addItem("1.3");
+
+    comboBox_2->clear();
+    comboBox_2->addItem("OP-1a"); // OP-Atom, OP-1a, OP-1b, OP-1c, OP-2a, OP-2b, OP-2c, OP-3a, OP-3b, OP-3c
+    comboBox_2->addItem("OP-1b");
+    comboBox_2->addItem("OP-1c");
+    comboBox_2->addItem("OP-2a");
+    comboBox_2->addItem("OP-2b");
+    comboBox_2->addItem("OP-2c");
+    comboBox_2->addItem("OP-3a");
+    comboBox_2->addItem("OP-3b");
+    comboBox_2->addItem("OP-3c");
+    comboBox_2->addItem("OP-Atom");
+
     QObject::connect(selectFileButton, SIGNAL(clicked()),
                      this, SLOT(open()));
+
+    QObject::connect(startPushButton, SIGNAL(clicked()), this, SLOT(checkMxfFile()));
 
 
 
@@ -27,16 +47,54 @@ void MxfSpecificParameters::open()
                                                     tr("Mxf files (*.mxf)"));
     selectedFileLineEdit->setText(fileName);
 
-    checkMxfFile();
 }
 
 void MxfSpecificParameters::checkMxfFile()
 {
+    std::stringstream ss {};
     const auto fileName = selectedFileLineEdit->text();
-    std::cout << "Check mxf file: " << fileName.toStdString() << std::endl;
+    if (fileName.isEmpty()){
+        ss << "File name is empty(). Error." << std::endl;
+        resultsTextBrowser->insertPlainText(QString::fromStdString(ss.str()));
+        return;
+    }
+    ss << "==========" << std::endl;
+    ss << "Check mxf file: " << fileName.toStdString() << std::endl << std::endl;
+
+    resultsTextBrowser->insertPlainText(QString::fromStdString(ss.str()));
+    ss.str("");
+
     mxf_info::MxfInfo mxf_info {fileName.toStdString()};
 
-    std::cout << "Format version: " << mxf_info.getFormatVersion() << std::endl;
-    std::cout << "Format profile: " << mxf_info.getFormatProfile() << std::endl;
+    // format version
+    ss << "----------" << std::endl;
+    const auto actualFormatVersion = mxf_info.getFormatVersion();
+    ss << "Actual Format version: " << actualFormatVersion << std::endl;
+    const auto expectedFormatVersion = comboBox->currentText();
+    ss << "Expected Format version: " << expectedFormatVersion.toStdString() << std::endl;
+    resultsTextBrowser->insertPlainText(QString::fromStdString(ss.str()));
+    ss.str("");
+    if (actualFormatVersion != expectedFormatVersion.toStdString()){
+        resultsTextBrowser->insertHtml("<b>Fail check! Not equal!</b><br><br>");
+    }
+    else {
+        resultsTextBrowser->insertHtml("<b>Check success! Equal!</b><br><br>");
+    }
 
+    // format profile
+    const auto actualFormatProfile = mxf_info.getFormatProfile();
+    ss << "Actual Format profile: " << actualFormatProfile << std::endl;
+    const auto expectedFormatProfile = comboBox_2->currentText();
+    ss << "Expected Format profile: " << expectedFormatProfile.toStdString() << std::endl;
+    resultsTextBrowser->insertPlainText(QString::fromStdString(ss.str()));
+    ss.str("");
+    if (actualFormatProfile != expectedFormatProfile.toStdString()){
+        resultsTextBrowser->insertHtml("<b>Fail check! Not equal!</b><br><br>");
+    }
+    else {
+        resultsTextBrowser->insertHtml("<b>Check success! Equal!</b><br><br>");
+    }
+
+    ss << "----------" << std::endl << std::endl;
+    resultsTextBrowser->insertPlainText(QString::fromStdString(ss.str()));
 }

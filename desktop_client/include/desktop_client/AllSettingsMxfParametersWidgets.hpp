45d164398b573be4a357047801e9b3e7e1f094e3
yurychu <yury_chugunov@mail.ru>
2020-10-26 18:41:56 +0300

    move to diff files settings classes

--- desktop_client/include/desktop_client/AllSettingsMxfParametersWidgets.hpp
+++ desktop_client/include/desktop_client/AllSettingsMxfParametersWidgets.hpp
@@ -1,29 +1,3 @@
 #pragma once
 
-#include <QWidget>
-#include <QLabel>
-#include <QCheckBox>
-
-#include <desktop_client/SaveableJSONWidget.hpp>
-#include <desktop_client/EnableCheckBox.hpp>
-
-
-/*
- * FormatVersionSettings
- */
-class FormatVersionSettings : public SaveableJSONWidget
-{
-    Q_OBJECT
-public:
-    explicit FormatVersionSettings(StructureSettingsSaver* saver, QWidget* parent=nullptr);
-    ~FormatVersionSettings() override = default;
-
-    static const QString& getKeyName();
-
-private slots:
-    void updateToObj();
-
-private:
-    EnableCheckBox* enableCheckBox;
-
-};
+#include <desktop_client/FormatVersionSettings.hpp>
---
+++ desktop_client/include/desktop_client/FormatVersionSettings.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <desktop_client/SaveableJSONWidget.hpp>
+#include <desktop_client/EnableCheckBox.hpp>
+
+
+/*
+ * FormatVersionSettings
+ */
+class FormatVersionSettings : public SaveableJSONWidget
+{
+Q_OBJECT
+public:
+    explicit FormatVersionSettings(StructureSettingsSaver* saver, QWidget* parent=nullptr);
+    ~FormatVersionSettings() override = default;
+
+    static const QString& getKeyName();
+
+private slots:
+    void updateToObj();
+
+private:
+    EnableCheckBox* enableCheckBox;
+
+};
